9a325b3d4ece0cb60b16584d0ee247682ac5a425
Nilver <nilverv@gmail.com>
2020-05-18 00:24:25 -0500

    args- final version

--- src/main/java/delivery/Base.java
+++ src/main/java/delivery/Base.java
@@ -2,6 +2,8 @@ package delivery;
 
 import delivery.delegate.DispaherDelegate;
 
+import javax.print.DocFlavor;
+
 /**
  * @author nilver
  * @version 1.0.0
@@ -10,9 +12,15 @@ import delivery.delegate.DispaherDelegate;
 public class Base {
 
     public static void main(String[] args) {
-        int totalDrones = 1;
+        int totalDrones = 4;
         int capacity = 3;
         int range = 10;
+
+        if(args.length==3){
+            totalDrones = Integer.parseInt(args[0]);
+            capacity = Integer.parseInt(args[1]);
+            range = Integer.parseInt(args[2]);
+        }
         new DispaherDelegate(totalDrones).init(range, capacity);
 
     }
